b6f2f03db8fd9ab25ef0a33863cf7ec24dfa5396
Nachiket Patel <159048874+now1die@users.noreply.github.com>
2026-09-15 01:45:27 +0530

    Update test_unlink.py

--- backend/test_unlink.py
+++ backend/test_unlink.py
@@ -1,1 +1,23 @@
+from vfs.filesystem import VirtualFileSystem
 
+
+vfs = VirtualFileSystem()
+
+vfs.create("/home", "directory")
+vfs.create("/home/test.txt", "file")
+
+vfs.link(
+    "/home/test.txt",
+    "/home/test_link.txt"
+)
+
+print("BEFORE UNLINK")
+print(vfs.get_all_inodes())
+
+vfs.unlink("/home/test.txt")
+
+print("AFTER UNLINK")
+print(vfs.get_all_inodes())
+
+print("REMAINING FILE")
+print(vfs.get_inode("/home/test_link.txt"))
